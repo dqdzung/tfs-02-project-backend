af8de9dd27236d4b3c3bf50474dab6f85d6a9525
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2021-06-24 16:32:39 +0700

    add DropTable func

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	db := database.ConnectDB()
 
+	db.Migrator().DropTable(&database.User{}, &database.Category{}, &database.Brand{}, &database.Product{}, &database.Order{}, &database.ProductOrder{})
+
 	db.AutoMigrate(&database.User{}, &database.Category{}, &database.Brand{}, &database.Product{}, &database.Order{}, &database.ProductOrder{})
 
 	fmt.Println("Done...")
