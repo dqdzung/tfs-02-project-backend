96d4ac610a81145466e9be3a4ad481fba66ea97c
HUY Nguyen Quang 1 (VTI.Internship) <huy.nguyenquang1@vti.com.vn>
2021-07-23 14:08:23 +0700

    commit

--- order/router/order_router.go
+++ order/router/order_router.go
@@ -1,15 +1,16 @@
 package router
 
 import (
-	"github.com/gorilla/mux"
 	"project-backend/middleware"
 	"project-backend/order/controller"
+
+	"github.com/gorilla/mux"
 )
 
 func OrderRouter(r *mux.Router) {
 	r = r.PathPrefix("/orders").Subrouter()
 
-	r.Methods("POST").Path("/").HandlerFunc(middleware.TokenAuth(controller.CreateOrder))
+	r.Methods("POST").Path("").HandlerFunc(middleware.TokenAuth(controller.CreateOrder))
 	r.Methods("GET").Path("/voucher/{code}").HandlerFunc(controller.GetVoucherByCode)
 	r.Methods("POST").Path("/cart").HandlerFunc(middleware.TokenAuth(controller.CheckCart))
 
--- util/validator/validator.go
+++ util/validator/validator.go
@@ -16,7 +16,7 @@ func CheckMail(data string) bool {
 	if !CheckLength(data, 255) {
 		return false
 	}
-	regex := "^[a-z][a-z0-9_\\.]{4,32}@[a-z0-9]{2,}(\\.[a-z0-9]{2,4}){1,2}$"
+	regex := "^[a-zA-Z][a-zA-Z0-9_\\.]{4,32}@[a-zA-Z0-9]{2,}(\\.[a-zA-Z0-9]{2,4}){1,2}$"
 	match, _ := regexp.MatchString(regex, data)
 	return match
 }
